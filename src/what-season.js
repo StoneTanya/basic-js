918ad290e5f84d36b17330b76d139145c6613998
Волкова Татьяна <tvolkova@excitekit.ru>
2025-11-09 15:34:36 +0300

    feat: resolve what-season task

--- src/what-season.js
+++ src/what-season.js
@@ -11,9 +11,26 @@ const { NotImplementedError } = require('../lib');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  *
  */
-function getSeason(/* date */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function getSeason(date) {
+  if (!date) return 'Unable to determine the time of year!';
+if (
+    !(date instanceof Date) ||
+    Object.getPrototypeOf(date) !== Date.prototype ||
+    Object.getOwnPropertyNames(date).length > 0 ||
+    Object.getOwnPropertySymbols(date).length > 0
+  ) {
+    throw new Error('Invalid date!');
+  }
+
+  if (isNaN(date.getTime())) {
+    throw new Error('Invalid date!');
+  }
+
+  const month = date.getMonth();
+  if (month === 11 || month === 0 || month === 1) return 'winter';
+  if (month >= 2 && month <= 4) return 'spring';
+  if (month >= 5 && month <= 7) return 'summer';
+  return 'autumn';
 }
 
 module.exports = {
